808c53120866822e005b2e3860290b7b614ab870
MuhammadHuzaifaSheikh <you@example.com>
2020-07-15 11:14:08 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,441 @@
+// Ch 1
+// Assignment 1
+
+// alert("Hello");
+
+
+// Assignment 1
+
+// Assignment 2
+
+// alert('happy coding');
+
+// Assignment 2
+
+
+
+
+
+// Assignment 3
+
+// alert('welcome to js');
+// alert('happy coding');
+// Assignment 2
+
+// Assignment 4
+// console.log("welcome to js");
+
+// Assignment 5
+
+// Assignment 6
+
+// No need///
+
+// Assignment 6
+
+
+// Assignment 7
+
+// No need///
+
+// Assignment 7
+
+
+
+// Ch 1
+// Ch 2
+
+// Assignment 1
+
+// var userName;
+// Assignment 1
+
+// Assignment 2
+
+// var myName;
+// Assignment 2
+
+
+
+
+// Assignment 3
+
+// var mess = 'Hello World';
+// alert(mess);
+// Assignment 3
+
+
+// Assignment 4
+
+// var names = 'JOHN';
+// var age = '15 year old';
+// var certificate = 'Oxferd';
+// alert(names);
+// alert(age);
+// alert(certificate);
+// Assignment 4
+
+
+// Assignment 5
+// var pizza  = 'pizza pizz '
+// alert(pizza)
+// Assignment 5
+
+
+// Assignment 6
+// var email = 'ex@gmail.com'
+// alert("My Email address is "+email);
+
+// Assignment 6
+// Assignment 7
+// var book = 'A'
+// alert("I learn "+book+" smarter learn to javascript");
+
+// Assignment 7
+
+
+
+
+// Assignment 8
+// document.write("Yah! I can write javascript in Browser");
+
+// Assignment 8
+
+
+// Assignment 9
+// alert("Hey! I am Assignment 9 ");
+
+// Assignment 9
+
+// Ch 2
+
+// Ch 3
+// Assignment 1
+// var myAge = 15;
+// alert('Hey! I am '+myAge+" old")
+
+// Assignment 1
+// Assignment 2
+var nTimes = 0;
+// alert("Youhave visited this site "+ ++nTimes+" times")
+
+// Assignment 2
+
+// Assignment 3
+// var birthYear = 1720;
+// document.write("My birth year is "+ birthYear);
+// Assignment 3
+
+// Assignment 4
+// var store = prompt("store name");
+// var username = prompt("your name");
+// var product = prompt("product name");
+// var quantity = prompt("quantity");
+// document.write(username +"ordered "+quantity+ product +"from"+store);
+// Assignment 4
+// Ch 3
+
+// Ch 4
+// Assignment 1
+// var var1;var var2;var var3
+// Assignment 1
+// Assignment 2
+
+// illegal
+// var 1hh;
+// var 3;
+// var %mm;
+// var (g;
+
+// illegal
+// legal
+// var hh;
+// var n3;
+// var $;
+// var _;
+
+// legal
+// Assignment 2
+
+// Assignment 3
+// no need
+// Assignment 3
+
+// Ch 4
+
+
+// Ch 5
+
+
+// Assignment 1
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// document.write("the sum of "+num1+" and" +num2+" = "+num1+num2)
+
+// Assignment 1
+
+
+// Assignment 2
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// document.write("the subtraction of "+num1+" and" +num2+" = "+num1-num2);
+// document.write("the division of "+num1+" and" +num2+" = "+num1/num2);
+// document.write("the multiplication of "+num1+" and" +num2+" = "+num1*num2);
+// document.write("the modulus of "+num1+" and" +num2+" = "+num1%num2);
+
+// Assignment 2
+
+
+// Assignment 3
+
+// No need
+// Assignment 3
+
+// Assignment 4
+// var cost = 600;
+// var q = 6;
+// document.write("total cost of this movie "+cost*q+ " for "+q + "tickets" )
+// Assignment 4
+
+
+// Assignment 5
+
+// no need
+// Assignment 5
+
+// Assignment 6
+
+// no need
+// Assignment 6
+
+// Assignment 7
+
+// no need
+// Assignment 7
+
+
+
+
+// Assignment 8
+
+// no need
+// Assignment 8
+
+
+// Assignment 9
+
+// no need
+// Assignment 9
+
+// Ch 5
+
+
+// Ch 6 to 9
+// Assignment 1
+
+// var a = a++;
+// console.log(a);
+// Assignment 1
+
+// Assignment 2
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// console.log(result);
+// Assignment 2
+
+// Assignment 3
+// var userName = prompt('Type your name');
+// console.log('As slam 0 alikum'+userName);
+// Assignment 3
+
+// Assignment 4
+// not display
+// Assignment 4
+
+// Assignment 5
+// var sub1 = prompt('type subject 1');
+// var sub2 = prompt('type subject 2');
+// var sub3 = prompt('type subject 3');
+//
+// var obt1 = prompt('type obtained marks 1');
+// var obt2 = prompt('type obtained 2 marks');
+// var obt3 = prompt('type obtained 3 marks');
+//
+// var totalMarks= 100;
+//
+// document.write(`
+// <h1>Subject total marks and percentage </h1>
+// <table>
+// <tr>
+// <td>`
+//     +sub1  +`</td><td>`+obt1+ `</td><td></td>
+// </tr>
+// <tr>
+// <td>\`
+//     +sub1  +\`</td><td>\`+obt1+ \`</td><td></td>
+// </tr>
+// </table>`)
+
+// Assignment 5
+
+
+// Ch 6 to 9
+
+// Ch 9 to 11
+
+// Assignment 1
+
+// var city = prompt('Type city name of the biggest city of Pakistan');
+// if (city==="karachi"){
+//     alert("Welcome to city of lights ");
+// }
+
+// Assignment 1
+// Assignment 2
+// var gender  = prompt("type your gender");
+// if (gender.toLocaleLowerCase()==='male'){
+//     alert('Good morning Sir! ')
+// }
+// else if (gender.toLocaleLowerCase()==='female'){
+//     alert('Good morning Madam! ')
+// }
+
+// Assignment 2
+// Assignment 3
+//
+// var color = prompt('Type color');
+// if (color.toLocaleLowerCase()==='red'){
+//     alert('Must Stop')
+// }
+// else if (color.toLocaleLowerCase()==='green'){
+//     alert('Move now')
+// }
+// else if (color.toLocaleLowerCase()==='yellow'){
+//     alert('Ready to move')
+// }
+
+
+// Assignment 3
+// Assignment 4
+//
+// var fuel = prompt('type your fuel value');
+// if (+fuel<0.25){
+//     alert('Please refill the fuel in your car')
+// }
+// else {
+//     alert('You are awesome')
+// }
+
+// Assignment 4
+// Assignment 5
+// var a = 4;
+// if (++a === 5){
+//     alert("given condition for variable a is true");
+// }
+
+// Assignment 5
+// Assignment 6
+// var totalMarks = prompt('Type your total Marks')
+// var obtainedMarks = prompt('Type your obtained Marks')
+//
+// var percentage = (+obtainedMarks/+totalMarks)*100;
+//
+// if (percentage>=80){
+//     document.write('Total Marks'+ totalMarks + `<br>`+'Obtained Marks'+ obtainedMarks+`<br>`+'Percentage'+ percentage+`<br>`+'Grade'+ "A-1"+`<br>`+'Remarks'+ "Excellent");
+//
+// }
+// else if (percentage<=70){
+//     document.write('Total Marks'+ totalMarks + `<br>`+'Obtained Marks'+ obtainedMarks+`<br>`+'Percentage'+ percentage+`<br>`+'Grade'+ "A"+`<br>`+'Remarks'+ "Good");
+//
+// }
+// else if (percentage<=60){
+//     document.write('Total Marks'+ totalMarks + `<br>`+'Obtained Marks'+ obtainedMarks+`<br>`+'Percentage'+ percentage+`<br>`+'Grade'+ "B"+`<br>`+'Remarks'+ "You need to improve");
+//
+// }
+// else if (percentage>60) {
+//     document.write(   'Total Marks'+ totalMarks + `<br>`+'Obtained Marks'+ obtainedMarks+`<br>`+'Percentage'+ percentage+`<br>`+'Grade'+ "Fail"+`<br>`+'Remarks'+ "Sorry you are out of our class ");
+// }
+
+// Assignment 6
+// Assignment 7
+
+// var game = prompt('Guess any number 1 to 4')
+//
+// if (+game===  Math.floor(Math.random()*4+1) ){
+//     alert('Correct , you are awesome')
+// }
+// else if (+game+1===Math.floor(Math.random()*4+1)){
+//
+//     alert('Close enough to the correct answer')
+// }
+// else {
+//     alert('wrong')
+// }
+
+
+
+// Assignment 7
+// Assignment 8
+
+// var num = prompt('check this number is divisible by 3');
+// var divisible = num%3;
+// if (divisible===0){
+//     alert('this number is divisible')
+// }
+// else {
+//     alert('this number are not divisible')
+// }
+// Assignment 8
+
+
+// Assignment 9
+// var num = prompt('Search the number is Even or Odd');
+// var isEven = num%2;
+// if (isEven===0){
+//     alert('this number is Even')
+// }
+// else {
+//     alert('this number is Odd')
+// }
+// Assignment 9
+// Assignment 10
+
+// var gender  = prompt("type your gender");
+// if (gender.toLocaleLowerCase()==='male'){
+//     alert('Good morning Sir! ')
+// }
+// else if (gender.toLocaleLowerCase()==='female'){
+//     alert('Good morning Madam! ')
+// }
+
+
+// Assignment 10
+// Assignment 11
+// var game = prompt('Guess any number 1 to 4')
+//
+// if (+game===  Math.floor(Math.random()*4+1) ){
+//     alert('Correct , you are awesome')
+// }
+// else if (+game+1===Math.floor(Math.random()*4+1)){
+//
+//     alert('Close enough to the correct answer')
+// }
+// else {
+//     alert('wrong')
+// }
+
+
+
+// Assignment 11
+
+
+
+
+// Ch 9 to 11
+
+
+
+
+
